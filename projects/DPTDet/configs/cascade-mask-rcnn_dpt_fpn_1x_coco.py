0473750dadbae54d924118b6067c701348c5f73c
Daniel Wang <dhw2@andrew.cmu.edu>
2024-01-29 01:19:48 -0500

    Update cascade-mask-rcnn_dpt_fpn_1x_coco.py

--- projects/DPTDet/configs/cascade-mask-rcnn_dpt_fpn_1x_coco.py
+++ projects/DPTDet/configs/cascade-mask-rcnn_dpt_fpn_1x_coco.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../../configs/_base_/models/cascade-mask-rcnn_r50_fpn.py',
+    #'../../../configs/_base_/models/cascade-mask-rcnn_r50_fpn.py',
     '../../../configs/_base_/datasets/coco_instance.py',
     '../../../configs/_base_/schedules/schedule_1x.py', '../../../configs/_base_/default_runtime.py'
 ]
