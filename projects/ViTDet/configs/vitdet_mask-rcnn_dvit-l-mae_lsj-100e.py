ac24a67934a65a95b98d31054d096cf1130fe320
Daniel Wang <dhw2@andrew.cmu.edu>
2024-02-20 00:03:03 -0500

    Update vitdet_mask-rcnn_dvit-l-mae_lsj-100e.py

--- projects/ViTDet/configs/vitdet_mask-rcnn_dvit-l-mae_lsj-100e.py
+++ projects/ViTDet/configs/vitdet_mask-rcnn_dvit-l-mae_lsj-100e.py
@@ -5,7 +5,7 @@ _base_ = [
 custom_imports = dict(imports=['projects.ViTDet.vitdet'])
 backbone_norm_cfg = dict(type='LN', requires_grad=True)
 norm_cfg = dict(type='LN2d', requires_grad=True)
-image_size = (512, 512)
+image_size = (518, 518)
 batch_augments = [
     dict(type='BatchFixedSizePad', size=image_size, pad_mask=True)
 ]
