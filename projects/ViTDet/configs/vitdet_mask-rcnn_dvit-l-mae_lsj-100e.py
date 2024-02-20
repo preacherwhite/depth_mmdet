67b592299a2a808d33ea2ac04f36fd34570b10ee
Daniel Wang <dhw2@andrew.cmu.edu>
2024-02-19 23:46:21 -0500

    Update vitdet_mask-rcnn_dvit-l-mae_lsj-100e.py

--- projects/ViTDet/configs/vitdet_mask-rcnn_dvit-l-mae_lsj-100e.py
+++ projects/ViTDet/configs/vitdet_mask-rcnn_dvit-l-mae_lsj-100e.py
@@ -2,6 +2,7 @@ _base_ = [
     '/media/home/dhwang/depth_mmdet/configs/_base_/models/mask-rcnn_r50_fpn_no_backbone.py',
     './lsj-100e_coco-instance.py',
 ]
+custom_imports = dict(imports=['projects.ViTDet.vitdet'])
 backbone_norm_cfg = dict(type='LN', requires_grad=True)
 norm_cfg = dict(type='LN2d', requires_grad=True)
 image_size = (512, 512)
