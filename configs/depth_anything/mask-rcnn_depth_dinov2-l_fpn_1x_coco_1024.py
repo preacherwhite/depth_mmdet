96430f3b119ac08491168440e712800ee096a8e5
Daniel Wang <55667243+preacherwhite@users.noreply.github.com>
2024-02-21 17:01:40 -0500

    new configs

---
+++ configs/depth_anything/mask-rcnn_depth_dinov2-l_fpn_1x_coco_1024.py
@@ -0,0 +1,48 @@
+_base_ = [
+    '../_base_/models/mask-rcnn_r50_fpn_no_backbone.py',
+    './coco_instance_dvit_1024.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+model = dict(
+    type='MaskRCNN',
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_mask=True,
+        pad_size_divisor=28),
+    backbone=dict(
+        type='DINOv2',
+        version='large',
+        freeze=False,
+        load_from='/media/home/dhwang/depth_mmdet/checkpoints/depth_anything_vitl14.pth'
+    ),
+    neck=dict(in_channels=[1024, 1024, 1024, 1024]))
+
+max_epochs = 12
+train_cfg = dict(max_epochs=max_epochs)
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+        end=1000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        _delete_=True,
+        type='AdamW',
+        lr=0.0001,
+        betas=(0.9, 0.999),
+        weight_decay=0.05))
