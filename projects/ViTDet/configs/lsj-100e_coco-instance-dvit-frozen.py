479a5ce6430a7431bf2cd90cd9ef098787320c88
Daniel Wang <dhw2@andrew.cmu.edu>
2024-02-23 22:09:23 -0500

    Update lsj-100e_coco-instance-dvit-frozen.py

--- projects/ViTDet/configs/lsj-100e_coco-instance-dvit-frozen.py
+++ projects/ViTDet/configs/lsj-100e_coco-instance-dvit-frozen.py
@@ -41,7 +41,7 @@ test_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=24,
+    batch_size=4,
     num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
