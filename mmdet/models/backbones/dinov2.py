bcf7e06defe59d243f782836cf4de12b7b305fb7
Daniel Wang <dhw2@andrew.cmu.edu>
2024-02-23 21:05:52 -0500

    Update dinov2.py

--- mmdet/models/backbones/dinov2.py
+++ mmdet/models/backbones/dinov2.py
@@ -1,135 +1,51 @@
-_base_ = [
-    '../../../configs/_base_/default_runtime.py',
-]
+import torch
+from mmengine.model import BaseModule
+from torch import nn
 
-# dataset settings
-dataset_type = 'CocoDataset'
-data_root = '/media/staging1/dhwang/coco2017/'
-image_size = (700, 700)
+from mmdet.registry import MODELS
 
-backend_args = None
 
-train_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=backend_args),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='RandomFlip', prob=0.5),
-    dict(
-        type='RandomResize',
-        scale=image_size,
-        ratio_range=(0.1, 2.0),
-        keep_ratio=True),
-    dict(
-        type='RandomCrop',
-        crop_type='absolute_range',
-        crop_size=image_size,
-        recompute_bbox=True,
-        allow_negative_crop=True),
-    dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
-    dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
-    dict(type='PackDetInputs')
-]
+@MODELS.register_module()
+class DINOv2(nn.Module):
+    """Use DINOv2 pre-trained models
+    """
 
-test_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=backend_args),
-    dict(type='Resize', scale=image_size, keep_ratio=True),
-    dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(
-        type='PackDetInputs',
-        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
-                   'scale_factor'))
-]
+    def __init__(self, version='large', freeze=False, load_from=None):
+        super().__init__()
+        if version == 'large':
+            self.dinov2 = torch.hub.load('/media/home/dhwang/depth_mmdet/torchhub/facebookresearch_dinov2_main',
+                                         'dinov2_vitl14', source='local', pretrained=False)
+        elif version == 'base':
+            self.dinov2 = torch.hub.load('/media/home/dhwang/depth_mmdet/torchhub/facebookresearch_dinov2_main',
+                                         'dinov2_vitb14', source='local', pretrained=False)
+        elif version == 'native':
+            self.dinov2 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14')
+        else:
+            raise NotImplementedError
 
-train_dataloader = dict(
-    batch_size=2,
-    num_workers=8,
-    persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=True),
-    dataset=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file='annotations/instances_train2017.json',
-        data_prefix=dict(img='train2017/'),
-        filter_cfg=dict(filter_empty_gt=True, min_size=32),
-        pipeline=train_pipeline))
+        if load_from is not None:
+            d = torch.load(load_from, map_location='cpu')
+            new_d = {}
+            for key, value in d.items():
+                if 'pretrained' in key:
+                    new_d[key.replace('pretrained.', '')] = value
+            self.dinov2.load_state_dict(new_d)
 
-val_dataloader = dict(
-    batch_size=1,
-    num_workers=2,
-    persistent_workers=True,
-    drop_last=False,
-    sampler=dict(type='DefaultSampler', shuffle=False),
-    dataset=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file='annotations/instances_val2017.json',
-        data_prefix=dict(img='val2017/'),
-        test_mode=True,
-        pipeline=test_pipeline))
-test_dataloader = val_dataloader
+        self.freeze = freeze
 
-val_evaluator = dict(
-    type='CocoMetric',
-    ann_file=data_root + 'annotations/instances_val2017.json',
-    metric=['bbox', 'segm'],
-    format_only=False)
-test_evaluator = val_evaluator
+    def forward(self, inputs):
+        B, _, h, w = inputs.shape
 
-optim_wrapper = dict(
-    type='AmpOptimWrapper',
-    constructor='LayerDecayOptimizerConstructor',
-    paramwise_cfg={
-        'decay_rate': 0.7,
-        'decay_type': 'layer_wise',
-        'num_layers': 24,
-    },
-    optimizer=dict(
-        type='AdamW',
-        lr=1e-4,
-        betas=(0.9, 0.999),
-        weight_decay=0.1,
-    ))
+        if self.freeze:
+            with torch.no_grad():
+                features = self.dinov2.get_intermediate_layers(inputs, 1)
+        else:
+            features = self.dinov2.get_intermediate_layers(inputs, 1)
 
-# 100 ep = 184375 iters * 64 images/iter / 118000 images/ep
-max_iters = 184375
-interval = 5000
-dynamic_intervals = [(max_iters // interval * interval + 1, max_iters)]
-param_scheduler = [
-    dict(
-        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=250),
-    dict(
-        type='MultiStepLR',
-        begin=0,
-        end=max_iters,
-        by_epoch=False,
-        # 88 ep = [163889 iters * 64 images/iter / 118000 images/ep
-        # 96 ep = [177546 iters * 64 images/iter / 118000 images/ep
-        milestones=[163889, 177546],
-        gamma=0.1)
-]
+        outs = []
+        for feature in features:
+            C = feature.shape[-1]
+            feature = feature.permute(0, 2, 1).reshape(B, C, h // 14, w // 14).contiguous()
+            outs.append(feature)
 
-train_cfg = dict(
-    type='IterBasedTrainLoop',
-    max_iters=max_iters,
-    val_interval=interval,
-    dynamic_intervals=dynamic_intervals)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
-
-default_hooks = dict(
-    logger=dict(type='LoggerHook', interval=50),
-    checkpoint=dict(
-        type='CheckpointHook',
-        by_epoch=False,
-        save_last=True,
-        interval=interval,
-        max_keep_ckpts=5))
-vis_backends = [
-    dict(type='LocalVisBackend'),
-    dict(type='TensorboardVisBackend')
-]
-visualizer = dict(
-    type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
-log_processor = dict(type='LogProcessor', window_size=50, by_epoch=False)
-
-auto_scale_lr = dict(enable=True,base_batch_size=64)
+        return outs[0]
