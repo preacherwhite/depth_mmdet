6ca43453e4425fd2ec71873bc7e6b99ec8c32b5d
Daniel Wang <daniel.wang.dhw33@yale.edu>
2024-03-07 14:34:53 -0500

    add files

---
+++ projects/ViTDet/configs/dvit_mask-rcnn_vit-large_depth_lvis.py
@@ -0,0 +1,43 @@
+_base_ = [
+    '/media/home/dhwang/depth_mmdet/configs/_base_/models/mask-rcnn_r50_fpn.py',
+    './lsj-100e_lvis-instance-dvit.py',
+]
+custom_imports = dict(imports=['projects.ViTDet.vitdet'])
+backbone_norm_cfg = dict(type='LN', requires_grad=True)
+norm_cfg = dict(type='LN2d', requires_grad=True)
+image_size = (700, 700)
+batch_augments = [
+    dict(type='BatchFixedSizePad', size=image_size, pad_mask=True)
+]
+
+# model settings
+model = dict(
+    data_preprocessor=dict(pad_size_divisor=28, batch_augments=batch_augments),
+    backbone=dict(
+        _delete_ = True,
+        type='DINOv2',
+        version='large',
+        freeze=False,
+        load_from='/media/home/dhwang/depth_mmdet/checkpoints/depth_anything_vitl14.pth'
+    ),
+    neck=dict(
+        _delete_=True,
+        type='SimpleFPN',
+        backbone_channel=1024,
+        in_channels=[256, 512, 1024, 1024],
+        out_channels=256,
+        num_outs=5,
+        norm_cfg=norm_cfg),
+    rpn_head=dict(num_convs=2),
+    roi_head=dict(
+        bbox_head=dict(
+            type='Shared4Conv1FCBBoxHead',
+            conv_out_channels=256,
+            norm_cfg=norm_cfg,
+            num_classes=1203),
+        mask_head=dict(
+            norm_cfg=norm_cfg,
+            num_classes=1203)))
+
+custom_hooks = [dict(type='Fp16CompresssionHook')]
+find_unused_parameters = True
